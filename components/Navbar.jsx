43c5813c9b06915a87cc304ec4eacabcda30faed
Rishabh <rishkwal@gmail.com>
2023-10-21 09:08:48 +0530

    add logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function ResponsiveNavbar() {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
@@ -17,11 +18,16 @@ function ResponsiveNavbar() {
       <AppBar sx={{backgroundColor: 'black'}} position="static">
         <Toolbar>
          <Link href="/" style={{textDecoration: 'none', color: 'white'}}>
-          <Typography class="link" variant="h6" >
-            <strong>
-                NotifHi
-            </strong>
-          </Typography>
+            <div
+                style={{
+                    position: 'relative',
+                    width: '100px',
+                    height: '40px',
+                    marginRight: '10px',
+                }}
+            >
+                <Image src="/logo.png" alt="NotifHi Logo" layout="fill" />
+            </div>
          </Link>
         </Toolbar>
       </AppBar>
